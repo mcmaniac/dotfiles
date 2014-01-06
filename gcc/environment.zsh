1987fdb3041577b8c640bfa96cd637c91760695f
Nils Schweinsberg <mail@nils.cc>
2014-01-06 18:06:15 +0100

    GCC environment variables

---
+++ gcc/environment.zsh
@@ -0,0 +1,8 @@
+#export LANG=de_DE.UTF-8
+#export LC_ALL=de_DE.UTF-8
+
+#export LD_LIBRARY_PATH="$HOME/.local/lib"
+#export LD_RUN_PATH="$HOME/.local/lib"
+export LDFLAGS="-L$HOME/.local/lib"
+export CPPFLAGS="-I$HOME/.local/include"
+export CFLAGS="-I$HOME/.local/include"
